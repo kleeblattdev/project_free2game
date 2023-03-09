a9d17e9511a63a8a346e734329f603a1bae084d1
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-03-09 21:33:22 +0100

    hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import RecentlyList from "../components/recentlylist/Recentlylist";
 import TopGamesBrowser from "../components/topGamesBrowser/TopGamesBrowser";
+import HeroSection from "../components/herosection/Herosection";
+import heroImg from "../image/HeroSection_img.png"
 
 const Home = () => {
     return ( 
         <div>
             <h1>Home</h1>
+            <HeroSection 
+            img={heroImg}
+            />
             <RecentlyList />
             <TopGamesBrowser />
         </div>
