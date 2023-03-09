d6648b65768d9bc65b232d3820ef77c3177b7f46
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-03-09 15:00:30 +0100

    started recentpage

--- src/pages/RecentlyAdded.jsx
+++ src/pages/RecentlyAdded.jsx
@@ -1,7 +1,11 @@
+/* library import */
+import {useState, useEffect} from "react"
+import { v4 as uuidv4 } from 'uuid'
+
 const RecentlyAdded = () => {
     return ( 
         <h1>RecentlyAdded-Test</h1>
-     );
+    );
 }
- 
+
 export default RecentlyAdded;
