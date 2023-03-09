a3820af05c0c4f1f62eeb95d73a1c69af0a9d5b1
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-03-09 10:43:18 +0100

    added header and new file for variables

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,26 @@
-const Header = () => {
-    return ( 
-        <header className="header">
-            <h1>Header</h1>
+/* library import */
+import {useState} from "react"
 
+/* file import */
+import Logo from "../../image/logo.svg"
+
+/* SCSS import */
+
+import "./Header.scss"
+
+const Header = () => {
+    const [searchTerm, setSearchTerm] = useState("")
+    
+    function searchGame(event){
+        setSearchTerm(event.target.value)
+    }
+
+    return ( 
+        <header id="header">
+            <img src={Logo} alt="FreeToGame logo" />
+            <input type="search" name="search" id="search" onChange={searchGame}/>
         </header>
     );
 }
- 
+
 export default Header;
