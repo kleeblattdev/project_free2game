ec483740ed815487f1a850230a6b1c5e46a8d12e
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-03-09 17:14:53 +0100

    propsDurchreichen

--- src/pages/DetailView.jsx
+++ src/pages/DetailView.jsx
@@ -8,35 +8,53 @@ import { useParams } from "react-router-dom";
 
 
 const DetailView = () => {
-   const params = useParams();   /* mit params.id  ansprechen */
-    
-  const [getData, setData] = useState([])
+    const params = useParams();   /* mit params.id  ansprechen */
+    console.log(params);
+    console.log(params.id);
+
+    const [getData, setData] = useState()
 
     useEffect(() => {
-        fetch(`https://www.freetogame.com/api/game?${params.id}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            setData(data)
-        })
-            
-    
-/*         .catch(error => {
-            console.log("--> Error <--")
-            console.log(error)}) */
+        fetch(`https://www.freetogame.com/api/game?id=${params.id}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setData(data)
+            })
 
-    },[]);
 
-    return ( 
+            .catch(error => {
+                console.log("--> Error <--")
+                console.log(error)
+            })
+
+    }, [params.id]);
+
+    return (
         <section>
-        <h1>DetailView</h1>
-        <DetailsItem 
-                    name="Name"
-        >
 
-        </DetailsItem>
-        </section>
-     );
-}
+            <DetailsItem
+                 imgEins={getData?.screenshots[0].image}
+                 title={getData?.title}
+                thumbnail={getData?.thumbnail}
+                platform={getData?.platform}
+                genre={getData?.genre}
+                url={getData?.freetogame_profile_url}
+                description={getData?.description}
+                  imgZwei={getData?.screenshots[1].image}
+                imgDrei={getData?.screenshots[2].image}   
+                developer={getData?.developer}
+                publisher={getData?.publisher}
+                release_date={getData?.release_date}
+                 os={getData?.minimum_system_requirements.os}
+                memory={getData?.minimum_system_requirements.memory}
  
+
+            >
+
+            </DetailsItem>
+        </section>
+    );
+}
+
 export default DetailView;
