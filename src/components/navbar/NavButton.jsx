236c1fb14fec6c2e7fa0eaf6b0714716deb2af64
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-03-09 23:06:21 +0100

    fixed menu Button

--- src/components/navbar/NavButton.jsx
+++ src/components/navbar/NavButton.jsx
@@ -1,18 +1,20 @@
 /* library import */
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 /* import SCSS */
 import "./NavButton.scss";
 
-
 const NavGameButton = ({link, icon, text}) => {
-    function handleClick(){
-        /* document.querySelector(".navButton").classList.add("redBg"); */
+    const [isActive, setActive] = useState(false)
+
+    const handleClick = () => {
+        setActive(!isActive)
     }
 
     return ( 
         <>
-        <Link to={link} className="navButton" id={icon} onClick={handleClick}><p>{text}
+        <Link to={link} className={isActive? "navButton redBg":"navButton"} id={icon} onClick={handleClick}><p>{text}
             </p></Link>
         </>
     );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,23 +8,25 @@ const Navbar = () => {
     function openNav() {
         document.querySelector("nav").style.width = "200px";
         document.querySelector("header").style.marginLeft = "200px";
-        document.querySelector(".hamburgerButton").style.display = "none";
+        document.querySelector(".menuButton").style.display = "none";
         document.querySelector(".closeBtn").style.display = "block";
         document.querySelector(".verticalLine").style.left = "200px";
     }
         function closeNav() {
         document.querySelector("nav").style.width = "70px";
         document.querySelector("header").style.marginLeft = "0";
-        document.querySelector(".hamburgerButton").style.display = "block";
+        document.querySelector(".menuButton").style.display = "block";
         document.querySelector(".closeBtn").style.display = "none";
         document.querySelector(".verticalLine").style.left = "70px";
     }
     console.log(document.querySelectorAll(".navButton"))
-    
+
     return ( 
         <nav className="navBar">
-            <div className="hamburgerButton" onClick={openNav}></div>
-            <div className="closeBtn" onClick={closeNav}></div>
+            <div id="menuContainer">
+                <div className="menuButton" onClick={openNav}></div>
+                <div className="closeBtn" onClick={closeNav}></div>
+            </div>
             <NavButton link="/" icon="home" text="Home"/>
             <NavButton link="/allgames" icon="games" text="All Games"/>
             <NavButton link="/recentlyadded" icon="recent" text="Recently Added"/>
