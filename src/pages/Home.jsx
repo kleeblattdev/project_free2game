9ff3b90e33f3d3f1b953b45d74356196590422e5
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-03-10 09:18:35 +0100

    fixed typo heroSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 /* component import */
 import RecentlyList from "../components/recentlylist/Recentlylist";
 import TopGamesBrowser from "../components/topGamesBrowser/TopGamesBrowser";
-import HeroSection from "../components/herosection/Herosection";
+import HeroSection from "../components/herosection/HeroSection";
 import heroImg from "../image/HeroSection_img.png"
 
 const Home = () => {
