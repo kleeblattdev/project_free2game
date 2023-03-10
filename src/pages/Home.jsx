8ffbb0717be1b96dd1fb5a70b5b115648b53ea7c
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-03-10 15:41:39 +0100

    added navbar import in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import RecentlyList from "../components/recentlylist/Recentlylist"
 import TopGamesBrowser from "../components/topGamesBrowser/TopGamesBrowser"
 import HeroSection from "../components/heroSection/HeroSection"
 import TopGamesPC from "../components/topGamesPC/TopGamesPC"
+import Navbar from "../components/navbar/Navbar"
 
 /* image import */
 import heroImg from "../image/HeroSection_img.png"
