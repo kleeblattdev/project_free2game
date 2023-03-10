1b01db84e5ed66e919bc48b8b0c0265f144b0306
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-03-10 13:42:16 +0100

    topPcGames

--- src/components/topGamesPC/TopGamesPC.jsx
+++ src/components/topGamesPC/TopGamesPC.jsx
@@ -1,7 +1,38 @@
+import { useState, useEffect } from "react";
+import GameItemNoDescription from "../gameItemNoDescription/GameItemNoDescription";
+import { v4 as uuidv4 } from 'uuid';
+
+
 const TopGamesPC = () => {
+    
+    const [recentData, setRecentData] = useState([])
+    const [limiter,setLimiter] = useState(4)
+
+    useEffect(() => {
+        fetch(`https://www.freetogame.com/api/games?platform=browser&sort-by=release-date&platform=pc`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            setRecentData(data.slice(0,limiter))
+        })
+    },[])
+    
     return ( 
         <section className="topGamesPc">
-            <h1>Top Games List</h1>
+            <h1>Top PC Games List</h1>
+
+            {recentData?.map((item) => {
+                return(
+                    <GameItemNoDescription 
+                    img={item.thumbnail}
+                    title={item.title}
+                    platform={item.platform}
+                    id={(item.id).toString()}
+                    genre={item.genre}
+                    key={uuidv4()}
+                    />
+                )
+            })}
 
         </section> 
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@
 import RecentlyList from "../components/recentlylist/Recentlylist"
 import TopGamesBrowser from "../components/topGamesBrowser/TopGamesBrowser"
 import HeroSection from "../components/heroSection/HeroSection"
+import TopGamesPC from "../components/topGamesPC/TopGamesPC"
 
 /* image import */
 import heroImg from "../image/HeroSection_img.png"
@@ -11,6 +12,7 @@ const Home = () => {
         <main>
             <HeroSection img={heroImg} text="Home"/>
             <RecentlyList />
+            <TopGamesPC />
             <TopGamesBrowser />
         </main>
     )}
