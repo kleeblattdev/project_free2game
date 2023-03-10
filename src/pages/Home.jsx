b98d78b95011a0d3aeabe96a456ee6e8a26b7f37
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-03-10 10:09:56 +0100

    added image import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,9 @@ import RecentlyList from "../components/recentlylist/Recentlylist"
 import TopGamesBrowser from "../components/topGamesBrowser/TopGamesBrowser"
 import HeroSection from "../components/heroSection/HeroSection"
 
+/* image import */
+import heroImg from "../../image/HeroSection_img.png"
+
 const Home = () => {
     return ( 
         <main>
