4c53ddced369898e9fb069619abe359d63bee830
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-03-10 09:24:18 +0100

    fixed netlify problem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,14 @@
 /* component import */
 import RecentlyList from "../components/recentlylist/Recentlylist";
 import TopGamesBrowser from "../components/topGamesBrowser/TopGamesBrowser";
-import HeroSection from "../components/herosection/HeroSection";
+import HeroSection from "../components/herosection/HeroSection"
 import heroImg from "../image/HeroSection_img.png"
 
 const Home = () => {
     return ( 
         <main>
             <h1>Home</h1>
-            <HeroSection 
-            img={heroImg}
-            />
+            <HeroSection img={heroImg}/>
             <RecentlyList />
             <TopGamesBrowser />
         </main>
