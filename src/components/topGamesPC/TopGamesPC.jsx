a6a35d6001cb505041bc39a174354dfafa7bcda8
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-03-13 12:18:42 +0100

    top PC games styling

--- src/components/topGamesPC/TopGamesPC.jsx
+++ src/components/topGamesPC/TopGamesPC.jsx
@@ -27,51 +27,51 @@ const TopGamesPC = () => {
             <div className="firstGame">
                 <p className="number1">1</p>
                 <div className="title1">
-                <p>{recentData[0]?.title}</p>
+                <p className="bigTitle1">{recentData[0]?.title}</p>
                 <Link to={`/detailview/${recentData[0]?.id}`} className="button">READ MORE</Link>
                     <div>
                         <img src={windowsLogo} alt="windows" />
-                        <p>{recentData[1]?.genre}</p>
+                        <p className="genre">{recentData[1]?.genre}</p>
                     </div>
                 </div>
-                <img src={recentData[0]?.thumbnail} alt="" />
+                <img className="gameLogo" src={recentData[0]?.thumbnail} alt="" />
             </div>
             
             <div className="secondGame">
-                <img src={recentData[1]?.thumbnail} alt="" />
+                <img className="gameLogo" src={recentData[1]?.thumbnail} alt="" />
                     <p className="number">2</p>
                 <div className="description">
                 <p>{recentData[1]?.title}</p>
                 <Link to={`/detailview/${recentData[1]?.id}`} className="button">READ MORE</Link>
                 <div className="logoSection">
                     <img src={windowsLogo} alt="windows" />
-                    <p>{recentData[1]?.genre}</p>
+                    <p className="genre">{recentData[1]?.genre}</p>
                 </div>
                 </div>
             </div>
             
             <div className="thirdGame">
-                <img src={recentData[2]?.thumbnail} alt="" />
+                <img className="gameLogo" src={recentData[2]?.thumbnail} alt="" />
                 <p className="number">3</p>
                 <div className="description">
                 <p>{recentData[2]?.title}</p>
                 <Link to={`/detailview/${recentData[2]?.id}`} className="button">READ MORE</Link>
                 <div className="logoSection">
                     <img src={windowsLogo} alt="windows" />
-                    <p>{recentData[2]?.genre}</p>
+                    <p className="genre">{recentData[2]?.genre}</p>
                 </div>
                 </div>
             </div>
             
             <div className="fourthGame">
-                <img src={recentData[3]?.thumbnail} alt="" />
+                <img className="gameLogo" src={recentData[3]?.thumbnail} alt="" />
                 <p className="number">4</p>
                 <div className="description">
                 <p>{recentData[3]?.title}</p>
                 <Link to={`/detailview/${recentData[3]?.id}`} className="button">READ MORE</Link>
                 <div className="logoSection">
                     <img src={windowsLogo} alt="windows" />
-                    <p>{recentData[3]?.genre}</p>
+                    <p className="genre">{recentData[3]?.genre}</p>
                 </div>
                 </div>
             </div>
