946ebeecf54d1e62b7c9839fcd43301dec4ddcdb
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-03-13 13:06:18 +0100

    keine ahnung was ich hier mache

--- src/components/dropDownFilter/DropDownFilter.jsx
+++ src/components/dropDownFilter/DropDownFilter.jsx
@@ -15,67 +15,38 @@ import DropDownItem from './DropDownItem'
 import "./DropDownFilter.scss"
 
 const DropDownFilter = () => {
-    const [getPlatform, setGetPlatform]=useState("")
-    const [getGenre, setGetGenre]=useState("")
-    const [getSortBy, setGetSortBy]=useState("")
 
-    function handlePlatform(){
-        setGetPlatform(platform.value)
-    }
 
-    function handleGenre(event){
-        setGetGenre(event.target.value)
-    }
-
-    function handleSortBy(event){
-        setGetSortBy(event.target.value)
-    }
-
-    console.log(getPlatform, getGenre, getSortBy)
     
     return (
     <section className="dropDownFilter">
-        <label htmlFor="platform">
-            <select name="platform" id="platform" multiple={true} onChange={handlePlatform}>
-                {platform.map((platform)=>{
-                    return (<DropDownItem
-                    key={uuidv4()}
-                    value={platform.value}
-                    label={platform.label}
-                    />
-                    )
-                })}
-            </select>
-        </label>
-        <label htmlFor="genre">
-            <select name="genre" id="genre" multiple={true} onChange={handleGenre}>
-                {genre.map((genre)=>{
-                    return (<DropDownItem
-                        key={uuidv4()}
-                        value={genre.value}
-                        label={genre.label}
-                        />)
-                })}
-            </select>
-        </label>
-        <label htmlFor="sortBy">
-            <select name="sortBy" id="sortBy" multiple={true} onChange={handleSortBy}>
-                {sortBy.map((sortBy)=>{
-                    return (<DropDownItem
-                        key={uuidv4()}
-                        value={sortBy.value}
-                        label={sortBy.label}
-                        />)
-                })}
-            </select>
-        </label>
-        <label htmlFor="test">
-            <Select
-            coloseMenuOnSelect={false} 
-            options={platform}
-            isMulti
-            onChange={handlePlatform}/>
-        </label>
+        <div id="platform">
+            {platform.map((platform)=>{
+                return <DropDownItem
+                key = {uuidv4()}
+                label = {platform.label}
+                value = {platform.value}
+                />
+            })}
+        </div>
+        <div id="genre">
+            {genre.map((genre)=>{
+                return <DropDownItem
+                key = {uuidv4()}
+                label = {genre.label}
+                value = {genre.value}
+                />
+            })}
+        </div>
+        <div id="sortBy">
+            {sortBy.map((sortBy)=>{
+                return <DropDownItem
+                key = {uuidv4()}
+                label = {sortBy.label}
+                value = {sortBy.value}
+                />
+            })}
+        </div>
     </section>
     );
 }
--- src/components/dropDownFilter/DropDownItem.jsx
+++ src/components/dropDownFilter/DropDownItem.jsx
@@ -1,9 +1,51 @@
+/* library import */
+import {useState} from "react"
+
 /* SCSS import */
 import "./DropDownItem.scss"
 
 const DropDownItem = ({value, label}) => {
+    const [userFilter, setUserFilter] = useState({
+        platform:[],
+        genre:[],
+        sortBy:[],
+        response:[],
+    })
+
+
+    const handleChange = (event) =>{
+        const {value, checked} = event.target
+        const {platform, genre, sortBy} = userFilter
+
+        console.log(`${value} is ${checked}`)
+
+        if (checked) {
+            setUserFilter({
+                platform:[...platform, value],
+                genre:[...genre, value],
+                sortBy: [...sortBy,value],
+                response: [...platform,...genre,...sortBy,value]
+            })}
+
+        else{
+            setUserFilter({
+                platform: platform.filter((event)=> event !== value),
+                genre: genre.filter((event)=> event !== value),
+                sortBy: sortBy.filter((event)=> event !== value),
+                response: platform.filter((event)=> event !== value) &&genre.filter((event)=> event !== value)&& sortBy.filter((event)=> event !== value),
+            })
+        }
+
+        console.log(userFilter.response)
+    }
+
     return ( 
-        <option value={value}>{label}</option>
+        <>
+        <label htmlFor={label}className="DropDownItem">{label}
+            <input type="checkbox" name={label} value={value} onChange={handleChange}/>
+            <span className="checkmark"></span>
+        </label>
+        </>
     );
 }
 
